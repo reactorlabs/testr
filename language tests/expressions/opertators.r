4b1dec59517efd2fe5006518959561a258344ca8
Petr Maj <peta.maj82@gmail.com>
2013-03-21 22:22:56 +0100

    binary division added
    ~ 1100 tests

--- language tests/expressions/opertators.r
+++ language tests/expressions/opertators.r
@@ -224,3 +224,74 @@ typeof(4+4i * @T)
 #!g T =    (TRUE # 1L # 2.1 # 3+2i)
 #!g V =    (10L # 4 # 5+2i)
 ((@V) * (@T)) == ((@T) * (@V))
+
+!# binary division ----------------------------------------------------------------------------------------------------
+
+#! binary div operator with logical
+#!g T =    (TRUE # 1L # 2.1       # 2+2i   # (2+2i))
+#!g V(T) = (1    # 1  # 0.4761905 # 0.5+2i # 0.25-0.25i)
+#!t @V
+TRUE / @T
+
+# binary div operator with logical - result type
+#!g T =    (TRUE      # 1L        # 2.1      # 3+2i)
+#!g V(T) = ("double" # "double" # "double" # "complex")
+#!t @V
+typeof(TRUE / @T)
+
+#! binary div operator with integer
+#!g T =    (TRUE # 1L # 2.1       # 2+2i   # (2+2i))
+#!g V(T) = (2    # 2  # 0.952381  # 1+2i # 0.5-0.5i)
+#!t @V
+2L / @T
+
+#! binary div operator with integer - result type
+#!g T =    (TRUE      #  1L       # 2.1      # 3+2i)
+#!g V(T) = ("double" # "double" # "double" # "complex")
+#!t @V
+typeof(2L / @T)
+
+#! binary div operator with double
+#!g T =    (TRUE # 1L   # 2.1 # 3+2i   # (3+2i))
+#!g V(T) = (10.5  # 10.5  # 5 # 3.5+2i # 2.423077-1.615385i)
+#!t @V
+10.5 / @T
+
+#! binary div operator with double - result type
+#!g T =    (TRUE     # 1L       # 2.1      # 3+2i)
+#!g V(T) = ("double" # "double" # "double" # "complex")
+#!t @V
+typeof(10.5 / @T)
+
+#! binary div operator with complex
+#!g T =    (TRUE # 1L     # 2.1         # 3+2i        # (3+2i))
+#!g V(T) = (4+4i  # 4+4i  # 4+1.904762i # 4+3.333333i # 4.615385+0.923077i)
+#!t @V
+4+4i / @T
+
+#! binary div operator with complex - result type
+#!g T =    (TRUE     # 1L       # 2.1      # 3+2i)
+#!t "complex"
+typeof(4+4i / @T)
+
+#! binary div operator does not work with raw or string
+#!e non-numeric argument to binary operator
+#!g T =    (TRUE     # 1L       # 2.1      # 3+2i)
+#!g V = ( as.raw(3) # "foo")
+@T - @V
+
+#! binary div operator is not commutative
+#!g T =    (TRUE # 1L # 2.1 # 3+2i)
+#!g V =    (10L # 4 # 5+2i)
+(@V - @T) != (@T - @V) 
+
+#! division by zero is not INF
+#!g V = (FALSE # 0L # 0 # 0+0i)
+#!g T(V) = (Inf # Inf # Inf # Inf+0i)
+#!t @T
+67 / @V
+
+#! division by zero in pure complex numebers is Nan+Inf
+#!t NaN+Infi
+67+2i / 0+0i
+
